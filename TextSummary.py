5d3b06445109976f85d1e42a8c8812d244a924eb
John Yin <yinyihengjohn@foxmail.com>
2023-12-30 19:58:34 +1000

    Filter useless information and provide 5 useful posts

--- TextSummary.py
+++ TextSummary.py
@@ -1,12 +1,14 @@
 import praw
 import os
 from openai import OpenAI
+
 from dotenv import load_dotenv
+
 
 load_dotenv() # load .env file in current library
 reddit = praw.Reddit('Text Summary Bot')
 subreddit  = reddit.subreddit("brisbane")
-
+valid_post_num  = 0 # check valid post number
 client = OpenAI(
     api_key = os.getenv("OPENAI_API_KEY"),
 )
@@ -19,14 +21,24 @@ client = OpenAI(
 
 
 # go over hot posts in subreddit
-for submission in subreddit.hot(limit=2):
-    print("Processing submission:", submission.title)
-    print("Processed text: ", submission.selftext)
-    print(submission)
-    print("---------------------------------------")
-    print("\n")
+for submission in subreddit.hot(limit=8): # 10 post now
+    if valid_post_num >= 5:
+        break
+    # print("Processing submission:", submission.title)
+    # print("Processed text: ", submission.selftext)
+    # print(submission)
+    # print("---------------------------------------")
+    # print("\n")
     # initial prompt
-    prompt = "Here is a piece of text: '{}'. Please summarize it and provide useful information, if no useful information just reply no.".format(submission.selftext[:500])
+
+
+    prompt = """Here is a piece of text: ###{}###. 
+    Please summarize it and provide useful information,
+    if the post is identified as a useless information, reply like: "no".
+    if no useful information, it's useless.
+    if the post include information like ###&#x200B###, it's useless.
+    if the post is calling for comments or feedback, it's useless.
+    if the post provide no useful information, it's useless.""".format(submission.selftext[:500])
 
     # sending prompt to gpt model
     response = client.chat.completions.create(
@@ -41,9 +53,14 @@ for submission in subreddit.hot(limit=2):
     )
 
     # print the response from the gpt
-    print(response.choices[0].message.content)
-    print("---------------------------------------")
+    gpt_response = response.choices[0].message.content.strip().lower()
+    if gpt_response.startswith("no") or gpt_response.replace('.', '') == "no":
+        print("Received 'No' response, skipping to next post...")
+    else:
+        valid_post_num += 1
+        print(gpt_response)
+        print("----------------------------")
 
 # Further update
-# If the post simply includes video, ignore it, read next post
-# IF the post provides no useful information, read next post
+# GPT loop, further filter?
+# other platform?
